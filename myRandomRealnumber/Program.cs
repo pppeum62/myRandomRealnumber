41dbbb6eea286887ff66f95063b24cb9acf1b69e
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-19 22:02:14 +0900

    0519 실습 일지 05번 문제

--- myRandomRealnumber/Program.cs
+++ myRandomRealnumber/Program.cs
@@ -10,6 +10,28 @@ namespace myRandomRealnumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int rand = (int) (random.NextDouble() * 100);
+
+            bool state = true;
+
+            while(state)
+            {
+                Console.Write("숫자를 입력해주세요 >> ");
+                int user = int.Parse(Console.ReadLine());
+
+                if (rand < user)
+                {
+                    Console.WriteLine(user + "보다는 작은 숫자입니다.\n");
+                } else if (rand > user)
+                {
+                    Console.WriteLine(user + "보다는 큰 숫자입니다.\n");
+                } else if (rand == user)
+                {
+                    Console.WriteLine("정답입니다!\n");
+                    state = false;
+                }
+            }
         }
     }
 }
